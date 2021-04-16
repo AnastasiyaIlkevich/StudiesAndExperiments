93dfc2958116a178276b92ee375272d85b0ad62a
Sirini <sirin199@yandex.ru>
2021-04-16 18:47:22 +0300

    lesson 17

--- Class/src/lesson14_15_16ClassAndObject/ClassAndObject.java
+++ Class/src/lesson14_15_16ClassAndObject/ClassAndObject.java
@@ -5,22 +5,14 @@ public class ClassAndObject {
     public static void main(String[] args) {
 
         Person person1 = new Person();
-        person1.name = "Anastasiya";
-        person1.age = 30;
-        person1.say = "I study java!";
-
-        Person person2 = new Person();
-        person2.name = "Sasha";
-        person2.age = 32;
-        person2.say = "I study dance!";
-
-
+        person1.setName("Anastasiya");
+        person1.setAge(30);
+        person1.setSay("I study java!");
+        System.out.println(person1.getName() + ", " + person1.getAge() + ", " + person1.getSay());
 
         person1.speak();
-        System.out.println("до пенсии осталось "+ person1.egeConklueter()+" года");
+        System.out.println("до пенсии осталось " + person1.egeConklueter() + " года");
         System.out.println();
-        person2.speak();
-        System.out.println("до пенсии осталось "+ person2.egeConklueter()+" года");
 
     }
 
@@ -28,16 +20,48 @@ public class ClassAndObject {
 
 class Person {
 
-    String name;
-    int age;
-    String say;
+    private String name;
+    private int age;
+    private String say;
 
-    void speak(){
+    public void setAge(int age1) {
+        if (age1 < 0) {
+            System.out.println("ошибка. Проверте ввод возраста пользователя");
+        } else {
+            age = age1;
+        }
+    }
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setSay(String say1) {
+        say = say1;
+    }
+    public String getSay() {
+        return say;
+    }
+
+
+    public void setName(String username) {
+        if (username.isEmpty()) {
+            System.out.println("ошибка. Проверте ввод имени пользователя");
+        } else {
+            name = username;
+        }
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    void speak() {
         System.out.println(name + " ---> " + age + " лет " + say);
     }
 
-    int egeConklueter(){
-        int years = 65-age;
+    int egeConklueter() {
+        int years = 65 - age;
         return years;//возвращает и прекращает работу метода
     }
 }
